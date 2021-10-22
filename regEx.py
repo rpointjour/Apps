0a61b4264ee5e3996a5fbf1a1b178276fd9d1424
Rood Jerry Pointjour <54840122+rpointjour@users.noreply.github.com>
2021-10-22 00:17:03 -0400

    added regular expression for python

---
+++ regEx.py
@@ -0,0 +1,11 @@
+import re
+# Import regular expression library
+
+txt = 'abcdefg'
+# Initialize string variable "txt"
+
+m = re.search('abc', txt)
+# Use search function for re to find match
+
+print(m)
+# Display re result
